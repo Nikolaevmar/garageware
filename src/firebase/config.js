c0d7f2b541c92e9e4bc7966144f0aa621a5ac5a8
marchel <marioniko001@gmail.com>
2021-12-09 23:32:18 +0200

    moved assets folder and set up firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3S-Lz9BIH20i6XiuTEXYHAr-G9m9zC_U",
+    authDomain: "garagemanagement-53cbf.firebaseapp.com",
+    projectId: "garagemanagement-53cbf",
+    storageBucket: "garagemanagement-53cbf.appspot.com",
+    messagingSenderId: "337725820337",
+    appId: "1:337725820337:web:d2811c122f5c73edec4190"
+  };
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init service
+const firestore = firebase.firestore();
+const fireauth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { firestore, fireauth, timestamp };
