44079050584ea78e49e97314df3fd57e106a5da9
marchel <marioniko001@gmail.com>
2021-12-11 13:35:17 +0200

    add logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,18 +1,23 @@
 import { useState, useEffect } from "react";
-import { fireauth } from "../firebase/config";
+import { fireauth, firestore } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
-  const { dispatch } = useAuthContext();
+  const { dispatch, user } = useAuthContext();
 
   const logout = async () => {
     setError(null);
     setIsPending(true);
 
     try {
+  
+      //update status using the uid from our authcontext
+      const {uid} = user;
+      await firestore.collection('users').doc(uid).update({online: false})
+
       await fireauth.signOut();
       dispatch({ type: "LOG_OUT" });
 
