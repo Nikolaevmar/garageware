0956ae9e881ea29b5ec79d1629cbe95ad7fbea3f
marchel <marioniko001@gmail.com>
2021-12-13 23:23:27 +0200

    added a button for completing tasks

--- src/pages/Details/DetailsSummary.js
+++ src/pages/Details/DetailsSummary.js
@@ -1,10 +1,24 @@
+import {useFirestore} from '../../hooks/useFirestore'
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useNavigate } from 'react-router-dom';
 import Profile from '../../components/Profile'
 
 export default function DetailsSummary({project}) {
+
+    const { user } = useAuthContext();
+    const navigate = useNavigate();
+    const {deleteDocument} = useFirestore('projects');
+
+    const handleClick = (e) => {
+       deleteDocument(project.id)
+       navigate('/dashboard')
+    }
+
     return (
         <div>
             <div className='project-summary'>
-            <h2 className='page-title'>{project.name}</h2>
+            <h1 className='page-title'>{project.name}</h1>
+            <p>By {project.createdBy.displayName}</p>
             <p className='due-date'>Project due by {project.dueDate.toDate().toDateString()}</p>
             <p className='details'>{project.details}</p>
             <h4>Project is assigned to:</h4>
@@ -15,6 +29,9 @@ export default function DetailsSummary({project}) {
                 </div>
             ))}
             </div>
+            {user.uid === project.createdBy.id &&(
+                <button className="btn" onClick={handleClick}>Mark as Complete</button>
+            )}
             </div>
         </div>
     )
