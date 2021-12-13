66623f35bc8192144513d92583d8f4a84432e9ad
marchel <marioniko001@gmail.com>
2021-12-13 12:33:14 +0200

    set up error handles for the details page

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -10,8 +10,12 @@ export const useDocument = (collection, id) => {
         const ref = firestore.collection(collection).doc(id)
 
         const unsub = ref.onSnapshot((snapshot) => {
+            if(snapshot.data()){
             setDocument({...snapshot.data(), id: snapshot.id})
             setError(null)
+            }else{
+                setError('Project does not exist')
+            }
         }, (err) => {
             console.log(err.message);
             setError('Failed to get document')
--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -1,9 +1,24 @@
+import { useDocument } from '../../hooks/useDocument'
+import { useParams } from 'react-router'
+
 import './Details.scss'
 
 export default function Details() {
+
+    const { id } = useParams();
+    const {error, document} = useDocument('projects', id)
+
+    if(error){
+        return <div className='error'>{error}</div>
+    }
+
+    if(!document){
+        return <div className='loading'>Loading...</div>
+    }
+
     return (
-        <div>
-            Details
+        <div className='project-details'>
+            <h2>{document.name}</h2>
         </div>
     )
 }
